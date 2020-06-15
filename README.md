df76c5bad6942cb6062fad7dbea001df3fbe7f4b
moreant <moreant@foxmail.com>
2020-06-15 20:39:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# gdmec-android-network
+
+移动网络应用开发这门课的源码，而这门课大部分都是《精通ANDROID网络开发》这本书的，而这本书的代码大部分都是来源于网络的
+
+比如这里
+
+![image-20200615203828623](md_img/image-20200615203828623.png)
+
+![image-20200615203843559](md_img/image-20200615203843559.png)
+
+
+
+[地址](https://github.com/bxh7425014/BookCode/blob/8996b36e689861d55662d15c5db8b0eb498c864b/Google.Android.SDK%E5%BC%80%E5%8F%91%E8%8C%83%E4%BE%8B%E5%A4%A7%E5%85%A8-%E7%AC%AC1%E7%89%88/CH08/EX08_06/src/irdc/ex08_06/EX08_06.java)
+
+
+
+只要你会用 github / google 搜索，基本都能找到
---
+++ app/src/main/java/top/yeek/web/MainActivity.java
@@ -0,0 +1,75 @@
+package top.yeek.web;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mButton1;
+    private TextView mTextView1;
+    private ImageView mImageView1;
+    String uriPic = "https://www.baidu.com/img/baidu_sylogo1.gif";
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton1 = (Button) findViewById(R.id.myButton1);
+        mTextView1 = (TextView) findViewById(R.id.myTextView1);
+        mImageView1 = (ImageView) findViewById(R.id.myImageView1);
+
+        mButton1.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                /* 设定Bitmap于ImageView中 */
+                mImageView1.setImageBitmap(getURLBitmap());
+                mTextView1.setText("");
+
+            }
+        });
+    }
+
+    public Bitmap getURLBitmap() {
+        URL imageUrl = null;
+        Bitmap bitmap = null;
+        try {
+            /* new URL对象将网址传入 */
+            imageUrl = new URL(uriPic);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            /* 取得联机 */
+            HttpURLConnection conn = (HttpURLConnection) imageUrl
+                    .openConnection();
+            conn.connect();
+            /* 取得回传的InputStream */
+            InputStream is = conn.getInputStream();
+            /* 将InputStream变成Bitmap */
+            bitmap = BitmapFactory.decodeStream(is);
+            /* 关闭InputStream */
+            is.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+}
