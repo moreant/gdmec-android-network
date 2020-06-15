028f863777668d935c457cbea251d81cf4613d57
moreant <moreant@foxmail.com>
2020-06-15 21:59:23 +0800

    fix: 显示网页适配安卓10

--- app/src/main/java/top/yeek/web/MainActivity.java
+++ app/src/main/java/top/yeek/web/MainActivity.java
@@ -1,5 +1,16 @@
 package top.yeek.web;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -7,17 +18,6 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.EditText;
-
-import top.yeek.web.R;
 
 public class MainActivity extends Activity {
     private EditText mEdit = null;
@@ -28,23 +28,30 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads().detectDiskWrites().detectNetwork()
+                .penaltyLog().build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects().detectLeakedClosableObjects()
+                .penaltyLog().penaltyDeath().build());
+
         mEdit = (EditText)findViewById(R.id.myEdit1);
         mButton = (Button)findViewById(R.id.myButton1);
         mWeb = (WebView)findViewById(R.id.myWeb1);
 
         mWeb.getSettings().setJavaScriptEnabled(true);
         //mWeb.getSettings().setPluginsEnabled(true);
-
+        mWeb.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return true;
+            }
+        });
         mButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
                 //String strUrl = mEdit.getText().toString();
-                String strUrl = "https://webadvisor.ashland.edu/";
-                String strFile = "/sdcard/test.html";
-                if (!strUrl.startsWith("https://")) {
-                    strUrl = "https://" + strUrl;
-                }
-                getStaticPageByBytes(strUrl, strFile);
-                mWeb.loadUrl("file://" + strFile);
+                String strUrl = "https://wwww.baidu.com";
+                mWeb.loadUrl(strUrl);
             }
         });
     }
